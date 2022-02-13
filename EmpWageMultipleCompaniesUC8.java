ea90e50317f1f2d6b959dff5679fb21146d581ed
DineshDK2310 <dineshkumardk101199@gmail.com>
2022-02-13 09:57:26 -0800

    Addind:EmployeeWage Multiple CompanyUC8 File Added Successfully

---
+++ EmpWageMultipleCompaniesUC8.java
@@ -0,0 +1,46 @@
+package EmpWageProblem;
+
+public class EmpWageMultipleCompaniesUC8 {
+	
+public static void calculatingEmployeeWage(String company,int empRatePerHour,int numberOfWorkingDays,int maxHoursPerMonth) {
+		int empWorkingHour=0;
+		int totalWorkingPerHour=0;
+		int totalWorkingDay=0;
+
+		while(totalWorkingDay <= numberOfWorkingDays && totalWorkingPerHour < empRatePerHour) {
+			totalWorkingDay++;
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+
+			switch(empCheck) {
+			case 1:
+				empWorkingHour = 8;
+				break;
+			case 2 :
+				empWorkingHour = 4;
+				break;
+			default : 
+				empWorkingHour = 0;
+				break;
+			}
+
+			totalWorkingPerHour+=empWorkingHour;
+			System.out.println("Day :"+totalWorkingDay);
+			System.out.println("Total Employee working hour  is  : " + empWorkingHour);
+
+		}
+		int totalEmployeewage = totalWorkingPerHour * empRatePerHour;
+		System.out.println("Total Employee Wage is :"+totalEmployeewage);
+		System.out.println("Total working hours are  : " + totalWorkingPerHour);
+
+	}
+
+
+	public static void main(String[] args) {
+
+		System.out.println("TCS : ");
+		calculatingEmployeeWage("TCS", 120, 30, 10);
+		System.out.println("\nHCL : ");
+		calculatingEmployeeWage("HCL", 125, 8, 80);
+	}
+}
+
